5321a4bb58a12905945843803f0819ef10d00f6f
halithsn2000-lab <halithsn2000@gmail.com>
2026-09-26 06:01:51 +0100

    finished the undo and next button

--- script.js
+++ script.js
@@ -9,10 +9,75 @@ let color = document.getElementById('colorPicker').value;
 let clearButton = document.getElementById('clearCanvas');
 let brushSize = document.getElementById('brushSize').value;
 let history = [];
+let stepscounter = 0;
+let currentcanvas ;
+let undobutton = document.getElementById('undo');
+let nextbutton = document.getElementById('next');
+let undolist = [];
+let nextlist = [];
+let pop ;
+let dump;
+let counter = 0;
+
+
+
+
+nextbutton.addEventListener("click", () => {
+
+ if (nextlist.length < 0 || nextlist.length == 0) {
+        console.log("next");
+    }
+    else if (counter == 0) {
+        ctx.putImageData(nextlist[nextlist.length - 1], 0, 0);
+        dump = nextlist.pop();
+        dump = undolist.push(dump);
+        console.log(nextlist.length);
+    }
+    else {
+        nextlist = [];
+    }
+});
+
+
+
+
+
+
+
+
+
+
+undobutton.addEventListener("click", () => {
+    counter = 0;
+    if (undolist.length < 0 || undolist.length == 0) {
+        console.log("undo");
+    }
+    else if (undolist.length == 1) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        pop = undolist.pop();
+        nextlist.push(pop);
+    }
+    else{
+    ctx.putImageData(undolist[undolist.length - 2], 0, 0);
+    pop = undolist.pop();
+    nextlist.push(pop);
+
+    console.log(undolist.length);
+     
+
+    };
+ 
+
+
+    });
+
 
 
 
 clearButton.addEventListener("click", () => {
+    nextlist = [];
+    undolist = [];
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     brushSize = document.getElementById('brushSize').value;
 });
@@ -20,23 +85,19 @@ clearButton.addEventListener("click", () => {
 
 
 canvas.addEventListener("mousedown", (event) => {
+
+    counter ++;
     mouseDown = true;
     brushSize = document.getElementById('brushSize').value;
     color = document.getElementById('colorPicker').value;
 
 });
 
-canvas.addEventListener("mouseup", (event) => {
-    mouseDown = false;
-    brushSize = document.getElementById('brushSize').value;
-    color = document.getElementById('colorPicker').value;
 
 
-});
 
 
 canvas.addEventListener("mousemove", (event) => {
-
     color = document.getElementById('colorPicker').value;
     brushSize = document.getElementById('brushSize').value;
     const canvasRect = canvas.getBoundingClientRect();
@@ -59,8 +120,24 @@ canvas.addEventListener("mousemove", (event) => {
         lastY = mouseY;
     }
 
+
+
 });
 
+
+
+
+
+
+
+canvas.addEventListener("mouseup", (event) => {
+    mouseDown = false;
+    brushSize = document.getElementById('brushSize').value;
+    color = document.getElementById('colorPicker').value;
+    
+    undolist.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+    
+});
 
 
 
